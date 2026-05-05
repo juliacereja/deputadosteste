4fa6db96e3bf1c30587abea887ce60b43a051270
juliacereja <jcerejameinhardt@gmail.com>
2026-05-05 14:42:39 -0300

    Update deputados.py

--- deputados.py
+++ deputados.py
@@ -5,3 +5,7 @@ df = pd.read_csv('deputados_2022.csv')
 voluntario = st.selectbox("Escolha um voluntário", df["nome"])
 
 dados = df[df["nome"] == voluntario]
+st.write(dados)
+
+st.metric("Idade", dados["partido"].values[0])
+st.metric("Peso", dados["nome_civil"].values[0])
